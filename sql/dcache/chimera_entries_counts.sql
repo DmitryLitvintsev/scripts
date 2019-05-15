a31d0c6fc2e4ff0ce3e952efc91769e0cce5e0cb
Dmitry Litvintsev <litvinse@fnal.gov>
2019-05-15 11:10:17 -0500

    add volume to the count

--- sql/dcache/chimera_entries_counts.sql
+++ sql/dcache/chimera_entries_counts.sql
@@ -2,10 +2,11 @@
 --- this query counts link, dirs and files in chimera
 ---
 select date_trunc('day',imtime) as day,
-       sum(case when itype = 40960 then 1 else 0 end) as links, 
-       sum(case when itype = 16384 then 1 else 0 end) as dirs, 
-       sum(case when itype = 32768 then 1 else 0 end) as files 
-from 
-     t_inodes 
-group by day 
+       sum(case when itype = 40960 then 1 else 0 end) as links,
+       sum(case when itype = 16384 then 1 else 0 end) as dirs,
+       sum(case when itype = 32768 then 1 else 0 end) as files,
+       sum(case when itype = 32768 then isize else 0 end) as volume
+from
+     t_inodes
+group by day
 order by day asc;
