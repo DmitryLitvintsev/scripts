9e75122ede944b1ccd49ee426e016ead9a1b93f1
Dmitry Litvintsev <litvinse@uqbar.fnal.gov>
2024-08-28 14:09:58 -0500

    add scripts that kill p2p and pp tranfsres on pool group

---
+++ python/dcache/executor_pp.py
@@ -0,0 +1,323 @@
+#!/bin/env python
+from __future__ import print_function
+import argparse
+import errno
+import multiprocessing
+import os
+import re
+import socket
+import subprocess
+import sys
+import time
+import uuid
+import paramiko
+import yaml
+
+try:
+    import urlparse
+except ModuleNotFoundError:
+    import urllib.parse as urlparse
+
+printLock = multiprocessing.Lock()
+kinitLock = multiprocessing.Lock()
+UUID = str(uuid.uuid4())
+CONFIG_FILE = os.getenv("MIGRATION_CONFIG")
+if not CONFIG_FILE:
+    CONFIG_FILE = "migration.yaml"
+
+
+HOSTNAME = socket.gethostname() + ".fnal.gov"
+SSH_HOST = "fndca"
+SSH_PORT = 24223
+SSH_USER = "enstore"
+POOL_GROUP = "readWritePools"
+
+
+def execute_command(cmd):
+    """
+    Executes shell command
+
+    :param cmd: command string
+    :type cmd: str
+    :return: shell command return code
+    :rtype: int
+    """
+    p = subprocess.Popen(cmd,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE,
+                         shell=True)
+    output, errors = p.communicate()
+    rc = p.returncode
+    return rc
+
+
+KRB5CCNAME = "/tmp/krb5cc_root.migration-%s"%(UUID,)
+os.environ["KRB5CCNAME"] = KRB5CCNAME
+
+
+def kinit():
+    """
+    Create kerberos ticket for admin shell access
+    """
+
+    cmd = "/usr/bin/kinit -k host/%s"%(HOSTNAME)
+    execute_command(cmd)
+
+
+def get_shell(host=SSH_HOST, port=SSH_PORT, user=SSH_USER):
+    """
+    Admin shell
+    """
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname=host,
+                port=port,
+                username=user,
+                gss_auth=True,
+                gss_kex=True)
+    return ssh
+
+
+def execute_admin_command(ssh, cmd):
+    """
+    Execute admin shell command
+    """
+    stdin, stdout, stderr = ssh.exec_command(cmd)
+    return [i.strip().replace(r"\r","\n") for i in stdout.readlines() if i.strip().replace(r"\r", "\n") != ""]
+
+
+def is_cached(ssh, pnfsid):
+    """
+    Check if file is cached
+    """
+    result = execute_admin_command(ssh, "\sn cacheinfoof " + pnfsid)
+    if not result:
+        return False
+    else:
+        return True
+
+
+def get_locations(ssh, pnfsid):
+    result = execute_admin_command(ssh, "\sn cacheinfoof " + pnfsid)
+    if result:
+        return result[0].split()
+    else:
+        return []
+
+
+def mark_precious(ssh, pnfsid):
+    """
+    marks pnfsid on all locations as precious
+    """
+    result = execute_admin_command(ssh, "\sl " + pnfsid + " rep set precious " + pnfsid)
+    return True
+
+
+def mark_precious_on_location(ssh, pool, pnfsid):
+    """
+    marks pnfsid on a pool as precious
+    """
+    result = execute_admin_command(ssh, "\s " + pool + " rep set precious " + pnfsid)
+    print_message("Marked precious %s %s %s" % (pnfsid, pool, result, ))
+    return True
+
+
+def clear_file_cache_location(ssh, pool, pnfsid):
+    """
+    clear file cache location
+    """
+    result = execute_admin_command(ssh, "\sn clear file cache location  " + pnfsid + " " + pool)
+    print_message("Cleared file cache location %s %s %s" % (pnfsid, pool, result, ))
+    return True
+
+
+def get_precious_fraction(ssh, pool):
+    """
+    return fraction of precious data on pool
+    """
+    result = execute_admin_command(ssh, "\s " + pool + " info -a")
+    lines = [i.strip() for i in result]
+    percentage = 0
+    for line in lines:
+        if line.find("Precious") != -1:
+            percentage = float(re.sub("[\[-\]]","", line.split()[-1]))
+            break
+    return percentage
+
+
+def get_active_pools_in_pool_group(ssh, pgroup):
+    """
+    Get list of pools in a  pool group
+    """
+    result = execute_admin_command(ssh, "\s PoolManager  psu ls pgroup -a " + pgroup)
+    pools = []
+    hasPoolList = False
+    for line in result:
+        i = line.strip()
+        if not i:
+            continue
+        if i.strip().find("nested groups") != -1:
+            break
+        if i.strip().startswith("poolList :"):
+            hasPoolList = True
+            continue
+        if hasPoolList:
+            parts = i.split()
+            if parts[1].find("mode=disabled") != -1:
+                continue
+            pool = parts[0].strip()
+            pools.append(pool)
+    return pools
+
+
+def stage(ssh, pool, pnfsid):
+    """
+    Schedule restore of file by pnfsid on a pool
+    """
+    cmd = "\s " + pool + " rh restore " + pnfsid
+    result = execute_admin_command(ssh, cmd)
+
+
+def print_error(text):
+    """
+    Print text string to stderr prefixed with timestamp
+    and ERROR keyword
+
+    :param text: text to be printed
+    :type text: str
+    :return: no value
+    :rtype: none
+    """
+    with printLock:
+        sys.stderr.write(time.strftime(
+            "%Y-%m-%d %H:%M:%S",
+            time.localtime(time.time()))+" ERROR : " + text + "\n")
+        sys.stderr.flush()
+
+
+def print_message(text):
+    """
+    Print text string to stdout prefixed with timestamp
+    and INFO keyword
+
+    :param text: text to be printed
+    :type text: str
+    :return: no value
+    :rtype: none
+    """
+    with printLock:
+        sys.stdout.write(time.strftime(
+            "%Y-%m-%d %H:%M:%S",
+            time.localtime(time.time()))+" INFO : " + text + "\n")
+        sys.stdout.flush()
+
+
+def get_path(pnfsid):
+    """
+    Get path for pnfsid
+    Uses mounts pnfs to do that
+    """
+    path = None
+    with open("/pnfs/fnal.gov/usr/.(pathof)(%s)" % (pnfsid, ), "r") as fh:
+        path = fh.readlines()[0].strip()
+    return path
+
+
+class KinitWorker(multiprocessing.Process):
+    def __init__(self):
+        super(KinitWorker, self).__init__()
+        self.stop = False
+
+    def run(self):
+        while not self.stop:
+            with kinitLock:
+                kinit()
+                time.sleep(14400)
+
+
+class Worker(multiprocessing.Process):
+    """
+    This class is responsible for staging files into source dCache system
+    """
+    def __init__(self, pool, config):
+        super(Worker, self).__init__()
+        self.pool = pool
+        self.config = config
+
+    def run(self):
+        # admin shell
+        ssh = get_shell(self.config.get("admin").get("host", SSH_HOST),
+                        self.config.get("admin").get("port", SSH_PORT),
+                        self.config.get("admin").get("user", SSH_USER))
+        print_message(f"Doing {self.pool}")
+
+        pp = execute_admin_command(ssh, f"\s {self.pool} pp ls") 
+        for line in pp:
+            parts = line.strip().split()
+            request_id = parts[0]
+            rc = execute_admin_command(ssh,
+                                       f"\s {self.pool} pp remove {request_id}")
+        print_message(f"Done {self.pool}")
+
+def main():
+    """
+    main function
+    """
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description="This script cansels p2ps pn pool")
+
+    parser.add_argument(
+        "--pgroup",
+        help="Name of pool group")
+
+    args = parser.parse_args()
+
+    try:
+        with open(CONFIG_FILE, "r") as f:
+            configuration = yaml.safe_load(f)
+    except (OSError, IOError) as e:
+        if e.errno == errno.ENOENT:
+            print_error("Config file %s does not exist" % (CONFIG_FILE,))
+        sys.exit(1)
+
+    if not configuration:
+        print_error("Failed to load configuration %s" % (CONFIG_FILE,))
+        sys.exit(1)
+
+    if not args.pgroup:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+
+    print_message("**** START ****")
+
+
+    kinitWorker = KinitWorker()
+    kinitWorker.start()
+
+    ssh = get_shell(configuration.get("admin").get("host", SSH_HOST),
+                    configuration.get("admin").get("port", SSH_PORT),
+                    configuration.get("admin").get("user", SSH_USER))
+
+    pools = get_active_pools_in_pool_group(ssh,
+                                           args.pgroup)
+    ssh.close()
+
+    cpu_count = len(pools)
+    pools.remove("rw-gm2-pubstor2225-5")
+    workers = []
+    for pool in pools:
+        worker = Worker(pool, configuration)
+        workers.append(worker)
+        worker.start()
+
+    list(map(lambda x : x.join(14400), workers))
+
+    kinitWorker.stop = True
+    kinitWorker.terminate()
+
+    print_message("**** FINISH ****")
+
+
+if __name__ == "__main__":
+    main()
